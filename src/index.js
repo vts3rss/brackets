8cd7250efbb38d823d31ec553aad983de150f14f
Vadim Tissen <vadim.tisse@gmail.com>
2023-01-14 14:54:07 +0600

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = []
+  let dict = {}
+  for (let i of bracketsConfig) {
+    dict[i[0]] = i[1]
+  }
+
+  for (let i of str) {
+    if (dict[array.at(-1)] === i) {
+      array.pop(i)
+    } else {
+      array.push(i)
+    }
+  }
+
+  return array.length === 0 ? true : false
 }
